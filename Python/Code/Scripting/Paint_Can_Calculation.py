d3fd4f03b6758ba2da00076381042bef5582448f
Ankit Vishwakarma <hello.its.avk@gmail.com>
2022-09-08 21:39:40 +0530

    Create Paint_Can_Calculation.py

---
+++ Python/Code/Scripting/Paint_Can_Calculation.py
@@ -0,0 +1,22 @@
+#Write your code below this line 👇
+import math
+def paint_calc(height,width,cover):
+  area = height * width
+  no_of_cans = math.ceil(area / cover)
+  print(f"You'll need {no_of_cans} cans to paint {area} sq_meter of wall")
+
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
